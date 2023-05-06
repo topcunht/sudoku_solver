34bd224cf2d26a5b451f4a236092545082b57893
topcunht <topcu.nht@gmail.com>
2023-05-06 18:28:26 +0300

    first commit

---
+++ sudoku_solver.py
@@ -0,0 +1,57 @@
+from pprint import pprint
+def find_empty_space(puzzle):
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r, c
+    return None, None
+
+def is_valid(puzzle, guess, row, col):
+    row_val = puzzle[row]
+    if guess in row_val:
+        return False
+    col_val = []
+    
+    for i in range(9):
+        col_val.append(puzzle[i][col])
+        
+    if guess in col_val:
+        return False
+    
+    row_start = (row // 3) *3
+    col_start = (col // 3) *3
+    
+    for r in range(row_start, row_start+3):
+        for c in range(col_start, col_start+3):
+            if puzzle[r][c] == guess:
+                return False
+    return True
+
+def solve_sudoku(puzzle):
+    row,col = find_empty_space(puzzle)
+    if row is None:
+        return True
+    for guess in range(1,10):
+        if is_valid(puzzle, guess, row, col):
+            puzzle[row][col] = guess
+            if solve_sudoku(puzzle):
+                return True
+        puzzle[row][col] = -1 #reset the guess
+    return False
+
+if __name__ == '__main__':
+    example_board = [
+        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
+        [-1, -1, -1,   2, -1, -1,   -1, -1, 5],
+        [-1, -1, -1,   7, 1, 9,   -1, 8, -1],
+
+        [-1, 5, -1,   -1, 6, 8,   -1, -1, -1],
+        [2, -1, 6,   -1, -1, 3,   -1, -1, -1],
+        [-1, -1, -1,   -1, -1, -1,   -1, -1, 4],
+
+        [5, -1, -1,   -1, -1, -1,   -1, -1, -1],
+        [6, 7, -1,   1, -1, 5,   -1, 4, -1],
+        [1, -1, 9,   -1, -1, -1,   2, -1, -1]
+    ]
+    print(solve_sudoku(example_board))
+    pprint(example_board)
